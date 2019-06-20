c0186b88ef9617cb79d83f49c28bf84fad22dfbe
xcnkx <cnk_2806@me.com>
2019-06-20 17:25:29 +0900

    add new features

--- ex_checker.py
+++ ex_checker.py
@@ -1,35 +1,57 @@
 import pandas as pd
 import os
 import datetime
+import zipfile
+import re
 
 
+def check_sub_id(ex_number):
+    for fn in files:
+        if -1 != fn.find(ex_number):
+            s_fname = fn.split("_")
+            submitted_fnames.update({s_fname[0]: fn})
 
-print("Input the path to the folder wich has the uploaded ex files (ex:\"./uploaded\").")
+
+def check_exfiles(file_name):
+    id = file_name.split("_")
+    x = []
+    with zipfile.ZipFile(path + '/' + file_name) as existing_zip:
+        files_n = 0
+        for name in existing_zip.namelist():
+            m = re.findall(r'ex[0-9]*/p[0-9]*/\w*.pdf$', str(name).lower())
+            # m = re.findall(r"ex[0-9]*/\w*.pdf$", str(.name).lower())
+
+            if m:
+                files_n += 1
+        x.append(files_n)
+
+    x.append(id[0])
+    if files_n != p_n:
+        err_std.append(id[0])
+    return x
+
+
+print("Input the path to the folder which has the uploaded ex files (ex:\"./uploaded\").")
 print("path : ", end='')
 path = input()
 
 files = os.listdir(path)
 names = []
-id = []
+submitted_fnames = {}
+ids = []
 ex = []
 submitted_list = []
+err_std = []
 
-print("Input the Exercise number that you want to check (ex: \"Ex1\").")
-print("Ex number : ", end='')
+print("Input the Exercise number that you want to check and it's Problems numbers (ex: \"Ex1\" 1).")
+print("Ex number & P numbers: ", end='')
 
-ex_n = input()
+epn = input().split()
+ex_n = epn[0]
+p_n = float(epn[1])
 
-for file_name in files:
-    names.append(file_name.split("_"))
-for i in names:
-    del i[1:3]
-    if len(i) == 2:
-        id.append(i)
-
-for s_id in id:
-    if ex_n in s_id[1]:
-        submitted_list.append(s_id[0])
-
+check_sub_id(ex_n)
+print(ex_n)
 ex_n = ex_n[2:]
 
 c_name = "Ex. " + ex_n + ": Sub"
@@ -37,17 +59,27 @@ c_name = "Ex. " + ex_n + ": Sub"
 print("Input the date of the sheet \"std6_YenClass\" that you want to overwrite. (ex: YYYY-MM-DD)")
 print("Sheet date: ", end='')
 
-sheet_name = "std6_YenClass_"+input()+".xlsx"
+sheet_name = "std6_YenClass_" + input() + ".xlsx"
 
 df = pd.read_excel(sheet_name)
-for i in submitted_list:
-    df.loc[df['ID'] == i, c_name] = 1
-df.loc[df[c_name].isnull(), c_name] = 0
 
+for i in submitted_fnames.values():
+    sub_id = check_exfiles(i)
+    score = sub_id[0] / p_n
+    df.loc[df['ID'] == sub_id[1], c_name] = score
+
+df.loc[df[c_name].isnull(), c_name] = 0
 
 date = datetime.date.today()
 
-f_name  = "./std6_YenClass_" + str(date) + ".xlsx"
-df.to_excel(f_name,index = False)
+f_name = "./std6_YenClass_" + str(date) + "_auto_created" ".xlsx"
+df.to_excel(f_name, index=False)
 
-print("The new "+f_name+" file was created")
+print("The new " + f_name + " file was created")
+
+print("Students that may be checked:")
+
+for j in err_std:
+    print("Student ID: " + j)
+    with zipfile.ZipFile(path + '/' + submitted_fnames[j]) as existing_zip:
+        print(existing_zip.namelist())
